66021d607216dd2da53071debb2f4299ce47bc53
348122622 <348122622@qq.com>
2017-07-05 11:14:43 +0800

    生成热图，关联矩阵

--- srcs/1.py
+++ srcs/1.py
@@ -9,6 +9,7 @@ import pickle
 train_path = '..\\data1\\train'
 
 
+# 计时器
 def run_time(func):
     def wrapper(num):
         start = time.time()
@@ -19,6 +20,7 @@ def run_time(func):
     return wrapper
 
 
+# 导入数据
 @run_time
 def get_data(num):
     num = str(num)
@@ -28,6 +30,7 @@ def get_data(num):
     return data_feat, data_norm, data_fail
 
 
+# 合并三个文件，加标签
 def add_label(data_feat, data_norm, data_fail):
     data_feat["label"] = np.NaN
     norm_len = data_norm.shape[0]
@@ -38,16 +41,29 @@ def add_label(data_feat, data_norm, data_fail):
     for j in range(fail_len):
         data_feat.loc[(data_fail['startTime'][j] <= data_feat["time"]) &
                       (data_feat["time"] <= data_fail['endTime'][j]), "label"] = 1
-
+    # 舍去标签缺失列
+    return data_feat.dropna()
 
 if __name__ == '__main__':
     data_15, norm_15, fail_15 = get_data(15)
     data_21, norm_21, fail_21 = get_data(21)
-    add_label(data_15, norm_15, fail_15)
-    add_label(data_21, norm_21, fail_21)
-    data_15["label"].value_counts(dropna=False)
-    data_21["label"].value_counts(dropna=False)
+    data15 = add_label(data_15, norm_15, fail_15)
+    data21 = add_label(data_21, norm_21, fail_21)
+    # data_15["label"].value_counts(dropna=False)
+    # data_21["label"].value_counts(dropna=False)
 
-    # data_15["label"] = data_15["label"].fillna(-1)
-    # plt.plot(data_15['time'], data_15['label'])
-    # plt.show()
+    corrmat = data15.corr()
+    f, ax = plt.subplots(figsize=(12, 9))
+    sns.heatmap(corrmat, vmax=.8, square=True)
+    plt.xticks(rotation=90)
+    plt.yticks(rotation=0)
+
+    plt.figure()
+    k = 10
+    cols = corrmat.nlargest(k, 'label')['label'].index
+    cm = np.corrcoef(data15[cols].values.T)
+    sns.set(font_scale=1.25)
+    sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values,
+                xticklabels=cols.values)
+    plt.xticks(rotation=90)
+    plt.yticks(rotation=0)
